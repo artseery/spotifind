2172c3801a0de08a594546a1cbd7b53ab9f43220
RanetkaGod <artyom.serikbaev@gmail.com>
2021-01-08 22:28:24 +0300

    Refactor track card Add feature of recommendations (console)

--- src/components/TrackCard.vue
+++ src/components/TrackCard.vue
@@ -1,16 +1,70 @@
 <template>
-  <div>
-    <slot></slot>
+  <div class="trackCard" @click="getRecommendations">
+    <div class="trackCard-artists">
+      <div class="track-image-wrapper">
+        <img class="track-image" :src="track.album.images[1].url" alt="Обложка композиции"/>
+      </div>
+      <div class="track-info">
+        <div class="track-name-wrapper">
+          <span class="track-name">{{ track.name }}</span>
+        </div>
+        <div class="track-artist-wrapper">
+            <span class="track-artist" v-for="(artist, i) in track.artists" :key="artist.id">
+              <!--eslint-disable-next-line-->
+               {{ artist.name }}{{ i < track.artists.length - 1 ? ', ' : '' }}
+            </span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import spotifyApiMixin from "@/mixins/spotifyApiMixin";
+
 export default {
-name: "TrackCard"
+  name: "TrackCard",
+  mixins: [spotifyApiMixin],
+  props: {
+    track: Object
+  },
+  methods: {
+    getRecommendations: function () {
+      this.getRecommendationsData(this.track.id)
+    }
+  }
 }
 </script>
 
 <style lang="sass" scoped>
-  .trackCard-wrapper
-    border: 1px solid lightgrey
+.trackCard
+  border: 1px solid lightgrey
+
+  .trackCard-artists
+    height: 100%
+
+    .track-image-wrapper
+      width: 220px
+      height: 220px
+      overflow: hidden
+      display: flex
+      flex-direction: row
+      justify-content: center
+
+      .track-image
+        height: 100%
+        width: auto
+
+    .track-info
+      height: 50px
+      .track-artist-wrapper, .track-name-wrapper
+        white-space: nowrap
+        overflow: hidden
+        text-overflow: ellipsis
+
+        .track-artist
+          height: 100%
+
+  .track-image
+    width: 100%
 </style>
--- src/components/TrackList.vue
+++ src/components/TrackList.vue
@@ -1,23 +1,7 @@
 <template>
   <div class="trackList-wrapper">
     <div class="trackList" v-if="$store.state.foundResults">
-      <track-card v-for="track in $store.state.foundResults.tracks.items" :key="track.id" class="trackCard">
-        <div class="trackCard-artists">
-          <div class="track-image-wrapper">
-            <img class="track-image" :src="track.album.images[1].url" alt="Обложка композиции"/>
-          </div>
-          <div class="track-info">
-            <div class="track-name-wrapper">
-              <span class="track-name">{{ track.name }}</span>
-            </div>
-            <div class="track-artist-wrapper">
-            <span class="track-artist" v-for="(artist, i) in track.artists" :key="artist.id">
-              <!--eslint-disable-next-line-->
-               {{ artist.name }}{{ i < track.artists.length - 1 ? ', ' : '' }}
-            </span>
-            </div>
-          </div>
-        </div>
+      <track-card v-for="track in $store.state.foundResults.tracks.items" :key="track.id" class="trackCard" :track="track">
       </track-card>
     </div>
   </div>
@@ -45,34 +29,5 @@ export default {
     grid-auto-rows: 270px
     grid-gap: 10px
 
-    .trackCard
-      border: 1px solid lightgrey
 
-      .trackCard-artists
-        height: 100%
-
-        .track-image-wrapper
-          width: 220px
-          height: 220px
-          overflow: hidden
-          display: flex
-          flex-direction: row
-          justify-content: center
-
-          .track-image
-            height: 100%
-            width: auto
-
-        .track-info
-          height: 50px
-          .track-artist-wrapper, .track-name-wrapper
-            white-space: nowrap
-            overflow: hidden
-            text-overflow: ellipsis
-
-            .track-artist
-              height: 100%
-
-      .track-image
-        width: 100%
 </style>
