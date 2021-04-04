293697159cddd51c953f3de70d826173b2886530
RanetkaGod <artyom.serikbaev@gmail.com>
2021-04-04 15:55:50 +0300

    Fix for genres state keeper

--- src/store/store.js
+++ src/store/store.js
@@ -130,7 +130,7 @@ const store = new Vuex.Store({
             commit('setFilterValues', data.filters)
             commit('chooseActiveTrack', data.active_track)
             commit('updateRecommendations', data.recommendations)
-            commit('selectGenres', data.selected_genres)
+            commit('selectGenres', data.genres)
         },
         setGenres({ commit }, genres) {
             commit('setGenres', genres)
