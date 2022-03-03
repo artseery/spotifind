a56e92ec36e4167c5141422d6171a4eee167d3d1
Serikbaev Artyom <artyom.serikbaev@gmail.com>
2022-03-03 22:45:58 +0300

    Update arjs

--- src/components/ARImage.vue
+++ src/components/ARImage.vue
@@ -10,7 +10,7 @@
     <a-marker
         type="pattern"
         preset="custom"
-        url="https://raw.githubusercontent.com/artseery/spotifind/master/src/assets/pattern.patt?123990"
+        url="https://raw.githubusercontent.com/artseery/spotifind/master/src/assets/pattern.patt?123999"
     >
       <a-image src="#test" rotation="90 180 0"
                scale="1"
